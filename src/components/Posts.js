4619b4d5524e923231e51bd97377a57f28860b83
Bonilla, Sam <Sam.Bonilla@united.com>
2022-09-27 12:57:54 -0500

    Commented out Post.js

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,34 +1,36 @@
-import React from "react";
-import axios from "axios";
-import Card from 'react-bootstrap/Card';
+// import React from "react";
+// import axios from "axios";
+// import Card from 'react-bootstrap/Card';
 
-function Posts() {
-  const [posts, setPosts] = React.useState([]);
+// function Posts() {
+//   const [posts, setPosts] = React.useState([]);
 
-  React.useEffect(() => {
-    axios
-      .get("http://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        const firstTen=response.data.slice(0,10)
-        setPosts(firstTen)
-      })
-  }, []);
+//   React.useEffect(() => {
+//     axios
+//       .get("http://jsonplaceholder.typicode.com/posts")
+//       .then((response) => {
+//         const firstTen=response.data.slice(0,10)
+//         setPosts(firstTen)
+//       })
+//   }, []);
 
 
-  return (
-    <Card>
-      <Card.Body> 
-      <ul className="posts">
-         {posts.map((post) => (
-           <li className="post" key={post.id}>
-             <h4>{post.title}</h4>
-             <p>{post.body}</p>
-           </li>
-         ))}
-        </ul></Card.Body>
-        </Card>
-  );
-}  
-export default Posts;
+//   return (
+    
+//       <ul className="posts">
+//          {posts.map((post) => (
+//           <Card>
+//           <Card.Body> 
+//            <li className="post" key={post.id}>
+//              <h4>{post.title}</h4>
+//              <p>{post.body}</p>
+//            </li>
+//            </Card.Body>
+//         </Card>
+//          ))}
+//         </ul>
+//   );
+// }  
+// export default Posts;
 
   
