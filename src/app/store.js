a59cad8d4dad94d4c38bc77c352b164d895ce061
Bonilla, Sam <Sam.Bonilla@united.com>
2022-10-04 13:38:36 -0500

    adding files

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 
-export default configureStore({
+export default function configureStore({
   reducer: {
     counter: counterReducer,
   },
