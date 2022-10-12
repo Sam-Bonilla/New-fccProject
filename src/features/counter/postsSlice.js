0811e7e2f09eada071b971a26974cf15245184ea
Bonilla, Sam <Sam.Bonilla@united.com>
2022-10-12 10:02:01 -0500

    Updated postsSlice

--- src/features/counter/postsSlice.js
+++ src/features/counter/postsSlice.js
@@ -1,37 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
-
-// const postsSlice = 'postsSlice'
-
-// function Posts() {
-//   return {
-//     type: postsSlice
-//     info: PostsPage
-//   }
-// }
-
-
-
-
-const initialState = {
-  posts: [],
-};
-
-//Slices taken library project need to adapt them to posts
+import { createSlice, nanoid } from '@reduxjs/toolkit'
 
 const postsSlice = createSlice({
-  name: 'posts',
-  initialState,
+  name: 'Posts',
+  initialState: [],
   reducers: {
-    addPosts(state, action) {
-      state.posts.push(action.payload);
+    addTodo: {
+      reducer: (state, action) => {
+        state.push(action.payload)
+      },
+      prepare: (text) => {
+        const id = nanoid()
+        return { payload: { id, text } }
+      },
     },
-    deletePosts(state, action){
-      state.posts = state.posts.filter((posts)=> posts.id !== action.payload.id);
-    }
   },
-});
+})
 
 
-export const { addPost, deletePosts } = postsSlice.actions;
 
-export default postsSlice.reducer
+  //   function createSlice({
+
+  //     // A name, used in action types
+  //     name: string,
+
+  //     // The initial state for the reducer
+  //     initialState: any,
+
+  //     // An object of "case reducers". Key names will be used to generate actions.
+  //     reducers: Object<string, ReducerFunction | ReducerAndPrepareObject>
+
+  //     // A "builder callback" function used to add more reducers, or
+  //     // an additional object of "case reducers", where the keys should be other
+  //     // action types
+  //     extraReducers?:
+  //     | Object<string, ReducerFunction>
+  //     | ((builder: ActionReducerMapBuilder<State>) => void)
+  // })
