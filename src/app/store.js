0bc438987587d7f21d5f66fc5a9b84f1d51f7177
Bonilla, Sam <Sam.Bonilla@united.com>
2022-10-13 16:34:57 -0500

    Added Reducer to Store

--- src/app/store.js
+++ src/app/store.js
@@ -2,8 +2,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import postsReducer from '../features/counter/postsSlice'
 
+//Add postsReducer
+
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: counterReducer, postsReducer,
   },
 })
+
+
+// export default configureStore({
+//   reducer: {
+//     posts: postsReducer,
+//   },
+// })
