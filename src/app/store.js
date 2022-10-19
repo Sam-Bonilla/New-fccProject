0f4555f15ee4d9cdff34ced6fb16c78890b2eb90
GithubReviewer <github_reviewer@outlook.com>
2022-10-19 13:49:53 -0500

    Fixed the White page issue

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
-import postsReducer from '../features/counter/postsSlice'
+import postsReducer from '../components/postsSlice'
 
 
 
