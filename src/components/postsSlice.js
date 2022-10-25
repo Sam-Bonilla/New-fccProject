6e92390d749c20f1dceec3c820c75de1fbc37fff
Sam-Bonilla <sambonilla@ymail.com>
2022-10-25 13:00:48 -0500

    Work in Progress revealing cards

--- src/components/postsSlice.js
+++ src/components/postsSlice.js
@@ -1,4 +1,6 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+// import { type } from '@testing-library/user-event/dist/type';
+// import axios from "axios";
 
 const postsSlice = createSlice({
   name: 'Posts',
@@ -13,9 +15,16 @@ const postsSlice = createSlice({
         text = "This is a test message";
         return { payload: { id, text } }
       },
+      Dispatch: (onClick) => {
+          const id = nanoid()
+          .get("http://jsonplaceholder.typicode.com/posts");
+          return { payload: { id,  } }
+        }
+      },
     },
   },
-})
+)
+
 export const { addPosts } = postsSlice.actions
 
 export const selectPosts = (state) => state?.posts?.value
