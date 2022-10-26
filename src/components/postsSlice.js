8bc1c835058fc9f87d89f0b4dad23a5dcd5b56df
Sam-Bonilla <sambonilla@ymail.com>
2022-10-26 14:03:20 -0500

    Editing PostSlice adding id's to Posts

--- src/components/postsSlice.js
+++ src/components/postsSlice.js
@@ -23,3 +23,18 @@ export const { addPosts } = postsSlice.actions
 export const selectPosts = (state) => state?.posts?.value
 
 export default postsSlice.reducer
+
+// import { createSlice } from '@reduxjs/toolkit'
+
+// const initialState = [
+//   { id: '1', title: 'First Post', content: 'This is a test message' },
+//   { id: '2', title: 'Second Post', content: 'This is the second test message' }
+// ]
+
+// const postsSlice = createSlice({
+//   name: 'posts',
+//   initialState,
+//   reducers: {}
+// })
+
+// export default postsSlice.reducer
