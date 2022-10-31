999ab4adef0bb553e2994c1af615c85e63166198
Sam-Bonilla <sambonilla@ymail.com>
2022-10-31 13:52:37 -0500

    Moved posts comp. to a new posts folder

---
+++ src/features/posts/postsSlice.js
@@ -0,0 +1,42 @@
+// import { createSlice, nanoid } from '@reduxjs/toolkit'
+
+
+// const postsSlice = createSlice({
+//   name: 'Posts',
+//   initialState: [],
+//   reducers: {
+//     addPosts: {
+//       reducer: (state, action) => {
+//         state.push(action.payload)
+//       },
+//       prepare: (text) => {
+//         const id = nanoid()
+//         text = "This is a test message";
+//         return { payload: { id, text } } },
+//         }
+//       },
+//     },
+// )
+
+
+
+// export default postsSlice.reducer
+
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = [
+  { id: '1', title: 'First Post', content: 'This is a test message' },
+  { id: '2', title: 'Second Post', content: 'This is the second test message' }
+]
+
+const postsSlice = createSlice({
+  name: 'posts',
+  initialState,
+  reducers: {}
+})
+
+export const { addPosts } = postsSlice.actions
+
+export const selectPosts = (state) => state?.posts?.value
+
+export default postsSlice.reducer
