9a4faf52475ed623be7512d093e67ee11c0df7bb
Sam-Bonilla <sambonilla@ymail.com>
2022-10-31 14:03:00 -0500

    Added postLists and Navbar to App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import Posts from "./components/Posts";
-import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Counter } from "./features/counter/Counter";
+// import Posts from "./components/Posts";
+// import "./App.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// // import { Counter } from "./features/counter/Counter";
+
+// // import { PostsList } from './features/posts/PostsList'
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src="/logo.svg" className="App-logo" alt="logo" />
+//         <h1>React Posts Sharer</h1>
+//       </header>
+//       <Posts />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom'
+
+import { Navbar } from './app/Navbar'
+
+import { PostsList } from './features/posts/PostsList'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <h1>React Posts Sharer</h1>
-      </header>
-      <Posts />
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <div className="App">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <React.Fragment>
+                <PostsList />
+              </React.Fragment>
+            )}
+          />
+          <Redirect to="/" />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
-
+export default App
