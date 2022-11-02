7a87231c74b0da05898345e478697f3985b0559c
Sam-Bonilla <sambonilla@ymail.com>
2022-11-02 15:22:30 -0500

    Displaying Posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 export const PostsList = () => {
-  const posts = useSelector(state => state.posts)
+  const posts = useSelector(state => state.post)
 
   const renderedPosts = posts?.map(post => (
     <article className="post-excerpt" key={post.id}>
