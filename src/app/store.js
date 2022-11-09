436b0782781bd9b249e1425810ab544d2399ef19
Sam-Bonilla <sambonilla@ymail.com>
2022-11-09 15:03:45 -0600

    Fixed the Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,52 +1,56 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import postsReducer from '../features/posts/postsSlice'
-// import createSagaMiddleware from "redux-saga";
-// import saga from "../saga.js";
-// import {
-//   createSlice,
-//   getDefaultMiddleware
-// } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import saga from "../saga.js";
+import {
+  createSlice,
+  getDefaultMiddleware
+} from "@reduxjs/toolkit";
 
 
 
-export default configureStore({
+// export default configureStore({
+//   reducer: {
+//     counter: counterReducer, 
+//     post: postsReducer,
+//       },
+//     });
+
+
+
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState: {
+    todos: []
+  },
   reducer: {
     counter: counterReducer, 
     post: postsReducer,
-      },
-    });
+  },
+  reducers: {
+    fetchData: (state, action) => {
+      return {
+        todos: action.payload
+      };
+    }
+  }
+});
 
+export const { fetchData } = todoSlice.actions;
 
+let sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
+const store = configureStore({
+  reducer: {
+    todo: todoSlice.reducer
+  },
+  middleware
+});
 
-// const todoSlice = createSlice({
-//   name: "todo",
-//   initialState: {
-//     todos: []
-//   },
-//   reducers: {
-//     fetchData: (state, action) => {
-//       return {
-//         todos: action.payload
-//       };
-//     }
-//   }
-// });
+sagaMiddleware.run(saga);
 
-// export const { fetchData } = todoSlice.actions;
-
-// let sagaMiddleware = createSagaMiddleware();
-// const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
-
-// const store = configureStore({
-//   reducer: {
-//     todo: todoSlice.reducer
-//   },
-//   middleware
-// });
-
-// sagaMiddleware.run(saga);
-
-// export default store;
+export default store;
 
