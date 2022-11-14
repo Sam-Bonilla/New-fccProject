ab6c9b592a259b814402524267485f6d25121191
Sam-Bonilla <sambonilla@ymail.com>
2022-11-14 13:47:34 -0600

    Adding Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from '../features/counter/counterSlice'
 import postsReducer from '../features/posts/postsSlice'
+import createSagaMiddleware from "redux-saga";
+import saga from "./src/saga.js";
+import {
+  // createSlice,
+  getDefaultMiddleware
+} from "@reduxjs/toolkit";
+
+// export default configureStore({
+//   reducer: {
+//     counter: counterReducer, 
+//     post: postsReducer,
+//   },
+// })
+
+let sagaMiddleware = createSagaMiddleware();
+const middleware = [...getDefaultMiddleware({ thunk: false }), sagaMiddleware];
 
 
+// let postsSlice = createSlice();
 
-export default configureStore({
+const store = configureStore({
   reducer: {
+    // post: postsSlice.reducer,
     counter: counterReducer, 
     post: postsReducer,
   },
-})
+  middleware
+});
+
+sagaMiddleware.run(saga);
+
+export default store;
